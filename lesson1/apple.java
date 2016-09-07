62bb30b8b84531bf6c78196c70503786d0d09731
Alvin Lo <alvinlocs@hotmail.com>
2016-09-07 11:10:52 -0700

    Added Files

---
+++ lesson1/apple.java
@@ -0,0 +1,12 @@
+public class apple
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" __ _");
+		System.out.println("/       ('>-");
+		System.out.println("\\__/");
+		System.out.println(" L\\");
+	
+		
+	}
+}
---
+++ lesson1/firstProg.java
@@ -0,0 +1,14 @@
+public class firstProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println("\t           ___");
+		System.out.println("\t          / . .\\");
+		System.out.println(" \t         \\  ---<");
+		System.out.println("\t         \\  /");
+		System.out.println("    __________/ /");
+		System.out.println("-=:__________/");
+		
+		
+	}
+}
