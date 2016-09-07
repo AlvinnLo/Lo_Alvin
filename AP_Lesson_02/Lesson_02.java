a46da61465cda88ba29c19208ef9877f867496f4
Alvin Lo <alvinlocs@hotmail.com>
2016-09-07 11:48:05 -0700

    Lesson 2 changes

---
+++ AP_Lesson_02/Lesson_02.java
@@ -0,0 +1,13 @@
+public class Lesson_02
+{
+	public static void main(String[]args)
+	{
+		String name = "ProfessorHandsome";
+		System.out.println(name);
+		int l = 8;
+		int w = 5;
+		int recArea = l * w;
+		
+		System.out.println("The area of your rectangle is" +" "+ recArea);
+	}
+}
