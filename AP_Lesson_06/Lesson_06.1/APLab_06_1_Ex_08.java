067f6848985822d7ec7459e26d8fc21383649819
AlvinnLo <yourfamilyishappy@gmail.com>
2016-10-31 11:40:59 -0700

    asddgh

---
+++ AP_Lesson_06/Lesson_06.1/APLab_06_1_Ex_08.java
@@ -0,0 +1,44 @@
+public class APLab_06_1_Ex_08
+{
+	public static void main(String[]args)
+	{
+		
+		String word = "na";
+		int repeat = 4;
+		
+		
+		for(int i=1; i <= repeat; i++)
+		{
+			System.out.print(word + " ");
+		}
+		
+		String word1 = "na";
+		int repeat1 = 4;
+		
+		
+		for(int i=1; i <= repeat1; i++)
+		{
+			System.out.print(word1 + " ");
+		}
+		
+		String word2 = "hey";
+		int repeat2 = 3;
+		
+		
+		for(int i=1; i <= repeat2; i++)
+		{
+			System.out.print(word2+ " ");
+		}	
+		
+	    String word3 = "goodbye";
+		int repeat3 = 1;
+		
+		
+		for(int i=1; i <= repeat3; i++)
+		{
+			System.out.print(word3+ " ");
+		}	
+		
+	}
+	
+}
