9882cca294281672415bedfd97882d6d8c46ecd3
AlvinnLo <yourfamilyishappy@gmail.com>
2016-11-17 11:14:44 -0800

    not done

---
+++ AP_Lesson_09/Lesson_09/APLab_09_Ex_04.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class APLab_09_Ex_04
+{
+	public static void main(String[]args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Please enter your starting number: ");
+		int start = keyboard.nextInt();
+		
+		System.out.println("Please enter your sequence size: ");
+		int size = keyboard.nextInt();
+		
+		int[] seq = new int[size];
+			
+			for (int i = 0; i < seq[i]; i++)
+		{
+			if ( i == 0 || i == 1)
+			{
+				seq[i] = start;
+			}
+			
+			else
+			{
+				seq[i] = seq[i-1] + seq [i-2];
+				
+			}
+			System.out.print( seq[i] + " " );	
+		}
+		
+	}
+}
