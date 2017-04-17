1628b1993b95248c7d7afc9f2c5b28c3af072f78
AlvinnLo <yourfamilyishappy@gmail.com>
2017-04-17 11:45:48 -0700

    041715

--- MagpieLab/Part 2/Magpie2.java
+++ MagpieLab/Part 2/Magpie2.java
@@ -59,11 +59,64 @@ public class Magpie2
 			response = "He sounds like a pretty dank teacher";
 		}
 		
+		else if (findKeyword(statement, "I want to", 0) >= 0)
+		{
+		  response = transformIWantToStatement(statement);
+		}
+
+
+		else
+		{
+		  // Look for a two word (you <something> me)
+		  // pattern
+		  int psn = findKeyword(statement, "you", 0);
+
+
+		  if (psn >= 0
+				&& findKeyword(statement, "me", psn) >= 0)
+		  {
+			 response = transformYouMeStatement(statement);
+		  }
 		else
 		{
 			response = getRandomResponse();
 		}
 		return response;
+	}
+	
+	private String transformIWantToStatement(String statement)
+	{
+		statement = statement.trim();
+		
+		String lastChar = statement.substring(statement.length() -1);
+		
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length()- 1);
+		}
+		int psn = findKeyword (statement, "I want to", 0);
+		
+		String restOfStatement = statement.substring(psn + 9).trim();
+		
+		return "What would it mean to " + restOfStatement + "?";
+	}
+	
+	private String transformYouMeStatement(String statement)
+	{
+		statement = statement.trim();
+		
+		String lastChar = statement.substring(statement.length()-1);
+		
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length() -1);
+		}
+		
+		int psnOfYou = findKeyword (statement, "you", 0);
+		int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
+		
+		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
+		return "What makes you think that I " + restOfStatement + " you?";
 	}
 
 	/** Ex_02: The findKeyword() Method...
@@ -107,35 +160,7 @@ public class Magpie2
 			}
 			
 		}
-		/* New String variable phrase = a more searchable version of statement.
-		 	-Use a combination of trim() and toLowerCase() modify statement.
 		
-
-		   New int variable psn = the location of goal in phrase after
-		   startPos
-
-			-->Refinement: Make sure we find goal by itself, and not part
-			of another word ("no" vs no in "know"). if you find an occurrence
-			of goal, make sure before and after aren't letters.
-
-			As long as psn >= 0...
-				Check if psn > 0 - there is no need to check for before at the
-				beginning of the word
-					set before = the slot in phrase before psn */
-
-				//====>code here
-
-				/*check if you can fit goal into the rest of phrase - no need to
-				proceed otherwise
-					set after = the slot in phrase after psn + length of goal */
-
-				//=====> code here
-
-				/* if before and after are not letters (compare before to "a"
-					and after to "z")
-						--return psn
-
-				Otherwise, search for goal in phrase from psn + 1 forward */
 
 		return -1;
 
